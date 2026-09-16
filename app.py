af24d8a2c8b966062b4ba52b90f6365680211a98
markunosov1 <markunosov1@rambler.ru>
2026-09-16 14:30:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,7 @@ def webhook():
                     
     return jsonify({"status": "ok"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __if __name__ == '__main__':
+    # Автоматическое считывание порта, который требует Render
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
