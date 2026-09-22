0ceb26bb990de6ea495740bc07296070187280d3
markunosov1 <markunosov1@rambler.ru>
2026-09-23 01:36:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -709,7 +709,7 @@ def normalize_candles(candles):
 # ============================================================
 # ТВОЯ СТРАТЕГИЯ
 # ============================================================
- def analyze_strategy(candles):
+def analyze_strategy(candles):
     if len(candles) < 20:
         return {
             "signal": "Нет сигналов",
